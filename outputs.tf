ee0987cd2115c05049a4306bc825ed3e034aaf95
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "mssql_managed_instance_transparent_data_encryptions" {
-  description = "All mssql_managed_instance_transparent_data_encryption resources"
-  value       = azurerm_mssql_managed_instance_transparent_data_encryption.mssql_managed_instance_transparent_data_encryptions
-}
 output "mssql_managed_instance_transparent_data_encryptions_auto_rotation_enabled" {
-  description = "List of auto_rotation_enabled values across all mssql_managed_instance_transparent_data_encryptions"
-  value       = [for k, v in azurerm_mssql_managed_instance_transparent_data_encryption.mssql_managed_instance_transparent_data_encryptions : v.auto_rotation_enabled]
+  description = "Map of auto_rotation_enabled values across all mssql_managed_instance_transparent_data_encryptions, keyed the same as var.mssql_managed_instance_transparent_data_encryptions"
+  value       = { for k, v in azurerm_mssql_managed_instance_transparent_data_encryption.mssql_managed_instance_transparent_data_encryptions : k => v.auto_rotation_enabled }
 }
 output "mssql_managed_instance_transparent_data_encryptions_key_vault_key_id" {
-  description = "List of key_vault_key_id values across all mssql_managed_instance_transparent_data_encryptions"
-  value       = [for k, v in azurerm_mssql_managed_instance_transparent_data_encryption.mssql_managed_instance_transparent_data_encryptions : v.key_vault_key_id]
+  description = "Map of key_vault_key_id values across all mssql_managed_instance_transparent_data_encryptions, keyed the same as var.mssql_managed_instance_transparent_data_encryptions"
+  value       = { for k, v in azurerm_mssql_managed_instance_transparent_data_encryption.mssql_managed_instance_transparent_data_encryptions : k => v.key_vault_key_id }
 }
 output "mssql_managed_instance_transparent_data_encryptions_managed_hsm_key_id" {
-  description = "List of managed_hsm_key_id values across all mssql_managed_instance_transparent_data_encryptions"
-  value       = [for k, v in azurerm_mssql_managed_instance_transparent_data_encryption.mssql_managed_instance_transparent_data_encryptions : v.managed_hsm_key_id]
+  description = "Map of managed_hsm_key_id values across all mssql_managed_instance_transparent_data_encryptions, keyed the same as var.mssql_managed_instance_transparent_data_encryptions"
+  value       = { for k, v in azurerm_mssql_managed_instance_transparent_data_encryption.mssql_managed_instance_transparent_data_encryptions : k => v.managed_hsm_key_id }
 }
 output "mssql_managed_instance_transparent_data_encryptions_managed_instance_id" {
-  description = "List of managed_instance_id values across all mssql_managed_instance_transparent_data_encryptions"
-  value       = [for k, v in azurerm_mssql_managed_instance_transparent_data_encryption.mssql_managed_instance_transparent_data_encryptions : v.managed_instance_id]
+  description = "Map of managed_instance_id values across all mssql_managed_instance_transparent_data_encryptions, keyed the same as var.mssql_managed_instance_transparent_data_encryptions"
+  value       = { for k, v in azurerm_mssql_managed_instance_transparent_data_encryption.mssql_managed_instance_transparent_data_encryptions : k => v.managed_instance_id }
 }
 
